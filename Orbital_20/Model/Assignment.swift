0634627d1ddbe60d38ae5d5e3cb7b4bed598c615
Lingy12 <russellgy1229@gmail.com>
2020-05-31 20:14:52 +0800

    update data model (without relationship)

---
+++ Orbital_20/Model/Assignment.swift
@@ -0,0 +1,18 @@
+//
+//  Assignment.swift
+//  Orbital_20
+//
+//  Created by Jerry Lin on 31/5/20.
+//  Copyright © 2020 Jerry Lin. All rights reserved.
+//  struct assignment is used to manage all the subtask under a certain assignment
+//  An assignment may or maynot have subtasks and itself has at least itself as a subtask
+
+import Foundation
+
+struct Assignment {
+   
+    
+    
+}
+
+
---
+++ Orbital_20/ViewModel/AssignmentViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  AssignmentViewModel.swift
+//  Orbital_20
+//
+//  Created by Jerry Lin on 31/5/20.
+//  Copyright © 2020 Jerry Lin. All rights reserved.
+//  Assignment viewmodel is used to to interect with the view
+
+import Foundation
+import SwiftUI
+
+
